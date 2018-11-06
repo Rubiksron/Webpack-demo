1c082bbf613cccf9ad128450445da6edfb12551c
Rubiksron <rubiksron@gmail.com>
2018-11-05 17:01:56 -0800

    reverted back to before redux, when things broke... bad.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-
 import Posts from './Posts'
 import PostForm from './Postform'
+
+
 
 class App extends Component {
   render() {
     return(
       <div>
-        <h1>my react app!</h1>
+        <h1>My React App! </h1>
         <PostForm />
         <hr />
         <Posts />
